9f157e49e46083c81225ada75cb7e3dc32af56b7
Omutnykh Oleg <omutnih1234@gmail.com>
2024-10-17 15:28:45 +0500

    Добавлены тесты на проверки времени

--- test1.py
+++ test1.py
@@ -1,4 +1,5 @@
-import unittest, prog1
+import unittest, time 
+import prog1
 
 class SquareEqSolverTestCase(unittest.TestCase):
     def correct_sort_b(self):
@@ -13,4 +14,20 @@ class SquareEqSolverTestCase(unittest.TestCase):
         res = prog1.selection_sort([8, 3, 1, 5])
         self.assertEqual(res, [1, 3, 5, 8])
 
-    
+    def correct_time_b(self):
+        t1 = time.time()
+        res = prog1.bubble_sort([8, 3, 1, 5])
+        t2 = time.time()
+        self.assertEqual(t2 - t1, 4)
+
+    def correct_time_q(self):
+        t1 = time.time()
+        res = prog1.quicksort([8, 3, 1, 5])
+        t2 = time.time()
+        self.assertEqual(t2 - t1, 4)
+        
+    def correct_time_s(self):
+        t1 = time.time()
+        res = prog1.selection_sort([8, 3, 1, 5])
+        t2 = time.time()
+        self.assertEqual(t2 - t1, 4)
